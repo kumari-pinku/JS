b45db5fdeb475ad6cf399c19bd37c5284a55f81e
Priyanka <priyankakcse56@gmail.com>
2023-07-03 14:00:38 +0530

    My first function in JS

--- Examples/Example-1/Functions.js
+++ Examples/Example-1/Functions.js
@@ -1,1 +1,17 @@
 // my first fucntion comment
+var a = 5;
+// Function declation
+function square(number) {
+  return number * number;
+}
+
+console.log(square(5)+"hhh");
+
+// function expression
+const square1 = function (number) {
+    return number * number;
+  };
+  
+  console.log(square1(4)); // 16
+  
+  {}
